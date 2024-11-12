ebf3e34e7ec3cf51fc8a13185588b9a73e5de91b
tomeian <maria.tomeyan70@gmail.com>
2024-11-12 12:24:59 +0100

    init

--- compress_images.py
+++ compress_images.py
@@ -11,12 +11,13 @@ def compress_images(input_folder, output_folder, max_size_mb=1):
             filepath = os.path.join(input_folder, filename)
             image = Image.open(filepath)
 
+
             quality = 95
             output_path = os.path.join(output_folder, filename)
             image.save(output_path, "JPEG", quality=quality)
 
-
-             while os.path.getsize(output_path) > max_size_mb * 1024 * 1024 and quality > 10:
+            # Compress image to fit under max_size_mb
+            while os.path.getsize(output_path) > max_size_mb * 1024 * 1024 and quality > 10:
                 quality -= 5
                 image.save(output_path, "JPEG", quality=quality)
 
